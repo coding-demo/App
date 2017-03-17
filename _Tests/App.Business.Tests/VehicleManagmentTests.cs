7a069c461065cd80a83fa839e6dc70a2feacd067
Neville <neville.lewis@gmail.com>
2017-03-16 21:00:32 -0500

    mocked repository layer for tests for business layer

--- _Tests/App.Business.Tests/VehicleManagmentTests.cs
+++ _Tests/App.Business.Tests/VehicleManagmentTests.cs
@@ -1,58 +1,54 @@
-﻿using App.Business;
-using App.DataModels;
-using App.Interfaces.Business;
+﻿using App.DataModels;
 using App.Interfaces.Repositories;
 using Moq;
-using System;
+using NUnit.Framework;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace App.Business.Tests
 {
     public class VehicleManagmentTests
     {
 
-
+        [Test]
         public void GetAllVehiclesTest()
         {
+            //Arrange
             var mockRepo = new Mock<IVehicleRepository>();
-            
-            mockRepo.Setup(vm => vm.GetAll()).Returns(mockRepo.Object.GetAll());
+            mockRepo.Setup(vm => vm.GetAll()).Returns(VehicleData());
+            var vehicleManagement = new VehicleManagment(mockRepo.Object);
 
-            var vehicleManagement = new VehicleManagment(mockRepo);
+            //Act
+            var rowCount = vehicleManagement.GetAllVehicles().Count;
+
+            //Assert
+            Assert.AreEqual(3, rowCount);
+        }
+
+        [Test]
+        public void GetVehicleByIdTest()
+        {
+            //Arrange
+            var mockRepo = new Mock<IVehicleRepository>();
+            mockRepo.Setup(vr => vr.FilterBy(It.Is<int>(i => i > 0))).Returns(VehicleData().Find(f => f.Id == 1));
+            var vehicleManagement = new VehicleManagment(mockRepo.Object);
+            int searchVehicleId = 1;
+
+            //Act
+            var data = vehicleManagement.GetVehicleBy(searchVehicleId);
+
+            //Assert
+            Assert.AreEqual(searchVehicleId, data.Id);
         }
 
 
-        //new List<Vehicle>()
-        //    {
-        //        new Vehicle() { Id = 1, Make = "Tesla", Model = "Model S", Year = 2017 },
-        //        new Vehicle() { Id = 1, Make = "Tesla", Model = "Model X", Year = 2017 },
-        //        new Vehicle() { Id = 1, Make = "Tesla", Model = "Energy", Year = 2017 },
-        //    }
-
-    //public class MockedVehicleRepository : IVehicleRepository
-    //{
-    //    public void DeleteVehicle(int id)
-    //    {
-    //        throw new NotImplementedException();
-    //    }
-
-    //    public Vehicle FilterBy(int id)
-    //    {
-    //        throw new NotImplementedException();
-    //    }
-
-    //    public List<Vehicle> GetAll()
-    //    {
-    //        throw new NotImplementedException();
-    //    }
-
-    //    public Vehicle SaveVehicleData(Vehicle data)
-    //    {
-    //        throw new NotImplementedException();
-    //    }
-    //}
-}
+        private List<Vehicle> VehicleData()
+        {
+            return new List<Vehicle>()
+                {
+                    new Vehicle() { Id = 1, Make = "Tesla", Model = "Model S", Year = 2017 },
+                    new Vehicle() { Id = 1, Make = "Tesla", Model = "Model X", Year = 2017 },
+                    new Vehicle() { Id = 1, Make = "Tesla", Model = "Energy", Year = 2017 },
+                };
+        }
+    }
 }
